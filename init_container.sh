2e1f87279c451d29c613239ce63c2aa8edcbd5e2
Laurent Mazuel <lmazuel@microsoft.com>
2017-06-20 10:52:31 -0700

    Use python to start the app

--- init_container.sh
+++ init_container.sh
@@ -4,7 +4,7 @@ service ssh start
 touch /home/LogFiles/python_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 echo "$(date) Container started" >> /home/LogFiles/python_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 
-# Should be outside
+# Should be outside in a "startup.sh" file
 
 pip install flask >> /home/LogFiles/python_${WEBSITE_ROLE_INSTANCE_ID}_out.log
-/home/site/wwwroot/app.py
+python /home/site/wwwroot/app.py
